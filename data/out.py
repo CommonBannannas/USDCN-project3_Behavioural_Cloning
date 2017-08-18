4ad6d90a176f9211db7469e675b74c74ebbb6a14
Sergio Santoyo <sergiosantoyo.92@gmail.com>
2017-08-18 15:55:55 -0500

    fixed files

--- data/out.py
+++
@@ -1,6 +0,0 @@
-# coding: utf-8
-for c in data.columns:
-    try:
-        data[c] = data[c].apply(lambda x: x.replace('/notebooks/USDCN-project3_Behavioural_Cloning/data/', ''))
-    except:
-        pass
